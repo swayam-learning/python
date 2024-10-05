27da29b7e441525a04dfc1c38fc6173244990f56
swayam-12205790 <swayamswarupbarik25@gmail.com>
2024-10-06 00:11:47 +0530

    prep for interview

--- databases/DML/Smartphones_DML.sql
+++ databases/DML/Smartphones_DML.sql
@@ -12,11 +12,11 @@ SELECT model , SQRT(resolution_width*resolution_width + resolution_height*resolu
 -- ----------------------------------Constants ----------------------------------------------------------------------------------------------
 
 SELECT model,"smartphone" AS "type" FROM smartphones_cleaned_v6;
--- ---------------------------------- Distinct ------------------------------------------------------------------------------------------------
+-- ---------------------------------- Distinct -----------------------------------------------------------------------------------------------
 SELECT DISTINCT (brand_name) AS "all_brands" FROM  smartphones_cleaned_v6;
 
 SELECT DISTINCT (processor_brand) AS "processor_name" FROM smartphones_cleaned_v6;
--- -------------------------------------------------------------------------------------------------------------------------------------------
+-- --------------------------------------------------------------------------------------------------------------------------------------------
 -- ---------------------------------Distinct combinations + Where -----------------------------------------------------------------------------
 SELECT DISTINCT brand_name,processor_brand from smartphones_cleaned_v6 WHERE brand_name="samsung";
 -- --------------------------------------------------------------------------------------------------------------------------------------------
@@ -24,4 +24,23 @@ SELECT * FROM smartphones_cleaned_v6 WHERE brand_name="samsung";
 
 SELECT * FROM smartphones_cleaned_v6 WHERE price > 60000 AND brand_name="samsung" ;
 -- ---------------------------------------------------------------------------------------------------------------------------------------------
+-- -------------------------------------Between-------------------------------------------------------------------------------------------------
+SELECT model FROM smartphones_cleaned_v6 WHERE price BETWEEN 10000 AND 20000;
+SELECT model ,rating FROM smartphones_cleaned_v6 WHERE rating BETWEEN 81 AND 24999;
+SELECT model , ram_capacity FROM smartphones_cleaned_v6 WHERE ram_capacity > 8;
+SELECT model ,price, processor_brand FROM smartphones_cleaned_v6 WHERE price BETWEEN 80 AND 25000 AND processor_brand ="snapdragon";
 
+-- ------------------------------Query Execution Order -----------------------------------------------------------------------------------------
+
+-- FROM --> JOIN --> WHERE --> GROUP BY --> HAVING --> SELECT --> DISTINCT --> ORDER BY
+
+-- ---------------------------------------------------------------------------------------------------------------------------------------------
+
+SELECT DISTINCT(brand_name) FROM smartphones_cleaned_v6 WHERE price >50000;
+
+-- --------------------------------IN and NOT IN -----------------------------------------------------------------------------------------------
+SELECT * FROM smartphones_cleaned_v6 WHERE processor_brand IN ('snapdragon','exynos','bionic');
+SELECT * FROM smartphones_cleaned_v6 WHERE processor_brand NOT IN ('snapdragon','exynos','bionic');
+-- ----------------------------------------------------------------------------------------------------------------------------------------------
+-- ------------------------UPDATE----------------------------------------------------------------------------------------------------------------
+UPDATE smartphones_cleaned_v6 SET processor_brand="dimensity" WHERE processor_brand="mediatek";
