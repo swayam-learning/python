0352a48ac3e3bfe5cde82e868cc9fccdb1f235d6
swayam-12205790 <swayamswarupbarik25@gmail.com>
2024-10-09 00:46:21 +0530

    prep for interview

--- databases_SQL/SUB-QUERIES/subqueries.sql
+++ databases_SQL/SUB-QUERIES/subqueries.sql
@@ -8,3 +8,20 @@ WHERE score = (SELECT MAX(score) FROM `movies (1)`);
 -- inner query result is acted upon the outer query
 
 -- --------------------------------------------------------------------------------------------------------------------
+-- Q1. Find the movie with highest profit (vs Order by)
+SELECT * FROM `movies (1)`
+ WHERE (gross - budget)=(SELECT MAX(gross-budget) FROM `movies (1)`);
+ -- O(n) + O(n) = O(2n)
+
+SELECT * FROM `movies (1)` ORDER BY (gross-budget) DESC LIMIT 1; 
+-- this will be faster in larger dataset
+
+ -- Q2. Find how many movies have a rating > the avg of all the movies ratings(Find the 
+ -- count of above average movies)
+SELECT * FROM `movies (1)` WHERE score > (SELECT AVG(score) FROM `movies (1)`);
+
+-- Q3. Find the highest rated movie in 2000
+SELECT * FROM `movies (1)` WHERE year=2000  AND score =(SELECT MAX(score) FROM `movies (1)` WHERE year=2000)
+
+-- Q4. Find the highest rated movie among all movies whose number of votes are > the dataset avg votes
+SELECT * FROM `movies (1)` WHERE score>(SELECT AVG(score) FROM `movies (1)`) 
