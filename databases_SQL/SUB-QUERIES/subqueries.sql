cf2cd7474838509f3f72039b8d8f604eba13c2a8
swayam-12205790 <swayamswarupbarik25@gmail.com>
2024-10-08 20:11:41 +0530

    prep for interview

--- databases_SQL/SUB-QUERIES/subqueries.sql
+++ databases_SQL/SUB-QUERIES/subqueries.sql
@@ -1,1 +1,5 @@
 -- Active: 1728126369033@@127.0.0.1@3306@sqllearning
+select * from `movies (1)`;
+
+-- ------------------------- Sub-Queries------------------------------------------------------------------------------
+SELECT * FROM `movies (1)` WHERE score = (SELECT MAX(score) FROM `movies (1)`);
